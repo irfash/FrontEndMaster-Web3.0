dfc5cb88161b3a057a00ffdf226da3527172cca8
Muhammed irfash khan <irfashfarook@gmail.com>
2022-01-22 10:05:14 +0530

    at deploy: first script

---
+++ scripts/deploy-hello.ts
@@ -0,0 +1,15 @@
+import "@nomiclabs/hardhat-ethers"
+import { ethers } from "hardhat";
+
+async function deploy() {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
+    return hello;
+}
+// @ts-ignore
+async function sayHello(hello) {
+    console.log("SayHello ", await hello.hello()) ;
+}
+
+deploy().then(sayHello);
